ab3758d135ca27c8316588a1b0eafba19513fdf7
Josh D King <joshdkingTX@gmail.com>
2017-03-10 16:07:25 -0600

    Testing a random comment added

--- app/src/main/java/heenan/letsrun/MainActivity.java
+++ app/src/main/java/heenan/letsrun/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//TTEEESSTTTT
         setContentView(R.layout.activity_main);
     }
 }
